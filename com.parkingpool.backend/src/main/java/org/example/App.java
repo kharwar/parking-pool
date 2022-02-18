1e78ddcab8f112ce124b0629e91a353e6431e39d
siddhxrthh <ksiddharth127@gmail.com>
2022-02-18 23:15:52 +0530

    samarth's branch initialized

--- com.parkingpool.backend/src/main/java/org/example/App.java
+++ com.parkingpool.backend/src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from Sid!" );
+        System.out.println( "Hello World from Samarth!" );
     }
 }
